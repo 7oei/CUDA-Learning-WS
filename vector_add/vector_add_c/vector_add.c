e772b4fa2ea5be8d574fdd655648905cbe321061
7oei <adachi-n@roboken.iit.tsukuba.ac.jp>
2021-09-30 14:42:23 +0900

    init

---
+++ vector_add/vector_add_c/vector_add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10000000
+void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+int main(){
+    float *a, *b, *out; 
+    // Allocate memory
+    a = (float*)malloc(sizeof(float) * N);
+    b = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+    // Main function
+    vector_add(out, a, b, N);
+    printf("%f\n", out[0]);
+}
